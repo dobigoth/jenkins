db5234cb49622f142d9674a330d4f25b39b059f8
dobigoth <circa1902@yahoo.com>
2025-10-22 19:44:47 +0800

    Initial commit of Terraform configuration

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "ap-southeast-1"  
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  instance_type = "t3.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
+}
